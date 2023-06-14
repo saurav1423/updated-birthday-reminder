d8516ee7548497f98e3662fde812c0cd8b285588
Saurav Kumar <55906348+saurav1423@users.noreply.github.com>
2023-06-15 01:53:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ require("./models/user");
 app.use(express.json());
 app.use(require("./routes/auth"));
 app.use(require("./routes/user"));
-// require('./nodemailer');
+require('./nodemailer');
 
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
